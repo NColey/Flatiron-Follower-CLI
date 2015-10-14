f67c1b5618a310c26390978495eae297844a0671
talum <tracidini@gmail.com>
2015-10-14 16:40:32 -0400

    Adds method to derive list of clean twitter handles

--- flatiron-twitter-cli/lib/scraper.rb
+++ flatiron-twitter-cli/lib/scraper.rb
@@ -36,10 +36,12 @@ def student_twitter_hash
 	#there are a couple people without correct names.
 end
 
+array_of_handles = student_twitter_hash.values.reject{|url| url == "https://twitter.com/"}.map{|url| url[20..-1]}
+binding.pry
 # def build_student_hash
 # 	count = 0
 # 	build_student_name_array.each_with_object({}) do |name, name_hash|
 # 		name_hash[name] = {page: get_student_profile_links[count], twitter_username: get_student_twitter_username[count]}
 # 		count +=1
 # 	end
-end
+#end
