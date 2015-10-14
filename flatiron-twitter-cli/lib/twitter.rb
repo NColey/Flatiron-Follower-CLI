cc3851dd45b120095257e696dcfe51811c354a7f
NColey <nrcoley@gmail.com>
2015-10-14 18:35:14 -0400

    updates scraper and twitter classes

--- flatiron-twitter-cli/lib/twitter.rb
+++ flatiron-twitter-cli/lib/twitter.rb
@@ -1,5 +1,6 @@
 require 'twitter'
 require 'yaml'
+require 'pry'
 
 class TwitterApi
   attr_reader :client
@@ -11,16 +12,32 @@ class TwitterApi
       config.consumer_secret     = keys['CONSUMER_SECRET']
       config.access_token        = keys['ACCESS_TOKEN']
       config.access_token_secret = keys['ACCESS_TOKEN_SECRET']
+    @scraper = Scraper.new
     end
   end
 
+  def follow_all(username_array)
+    client.follow(username_array)
+  end
 
+  def follow_classmate(username)
+    client.follow(username)
+  end
 
+  def tweet_at_classmate(tweet, username)
+    client.update(tweet)
+  end
 
+  def unfollow_all(username_array)
+    client.unfollow(username_array)
+  end
 
+  def unfollow_classmate(username)
+    client.unfollow(username)
+  end
 
+  def classmate_timeline(username)
+    client.user_timeline(username)
+  end
 
-
-
-  
 end
