7f00e8fe3d0c421ea5c9704a046e1e68fa75b182
NColey <ncoley>
2015-10-14 18:35:14 -0400

    updates scraper and twitter classes

--- flatiron-twitter-cli/lib/scraper.rb
+++ flatiron-twitter-cli/lib/scraper.rb
@@ -2,6 +2,8 @@ require 'pry'
 require 'Nokogiri'
 require 'open-uri'
 
+class Scraper
+
 	def noko_data
 		noko_document = Nokogiri::HTML(open("https://learn-co-students.github.io/deploy-on-day-1-web-0915/"))
 	end
@@ -14,38 +16,33 @@ require 'open-uri'
 		get_student_data('div.blog-title h3 a').collect {|name_data| name_data.text}
 	end
 
-	
-def student_page_array
-	students_name_array = get_student_data('div.blog-title h3 a').collect do |name_data|
-		"https://learn-co-students.github.io/deploy-on-day-1-web-0915/" + name_data['href']
+		
+	def student_page_array
+		students_name_array = get_student_data('div.blog-title h3 a').collect do |name_data|
+			"https://learn-co-students.github.io/deploy-on-day-1-web-0915/" + name_data['href']
+		end
+		
+		#cleans out the broken links
+		students_name_array.reject do |link|
+	    link == "https://learn-co-students.github.io/deploy-on-day-1-web-0915/students/student_name.html"
+	  end
+	  
 	end
 
-	students_name_array.reject do |link|
-    link == "https://learn-co-students.github.io/deploy-on-day-1-web-0915/students/student_name.html"
-  end
-  #cleans out the broken links
-end
-
-def student_twitter_hash
-	student_page_array.each_with_object({}) do |link, hash|
-    profile_html = open(link)
-    profile_doc = Nokogiri::HTML(profile_html)
-    name = profile_doc.search('.ib_main_header').text
-    twitter = profile_doc.search('.social-icons a')[0].attr('href') 
-    hash[name] = twitter 
+	def student_twitter_hash
+		student_page_array.each_with_object({}) do |link, hash|
+	    profile_html = open(link)
+	    profile_doc = Nokogiri::HTML(profile_html)
+	    name = profile_doc.search('.ib_main_header').text
+	    twitter = profile_doc.search('.social-icons a')[0].attr('href') 
+	    hash[name] = twitter 
+		end
+		#there are a couple people without correct names.
 	end
-	#there are a couple people without correct names.
+
+	hash_twitter_only = student_twitter_hash.reject{|name, url| url =="https://twitter.com/"}
+
+	def username_array
+		array_of_handles = student_twitter_hash.values.reject{|url| url == "https://twitter.com/"}.map{|url| url[20..-1]}
+	end
 end
-
-hash_twitter_only = student_twitter_hash.reject{|name, url| url =="https://twitter.com/"}
-
-array_of_handles = student_twitter_hash.values.reject{|url| url == "https://twitter.com/"}.map{|url| url[20..-1]}
-
-
-# def build_student_hash
-# 	count = 0
-# 	build_student_name_array.each_with_object({}) do |name, name_hash|
-# 		name_hash[name] = {page: get_student_profile_links[count], twitter_username: get_student_twitter_username[count]}
-# 		count +=1
-# 	end
-#end
