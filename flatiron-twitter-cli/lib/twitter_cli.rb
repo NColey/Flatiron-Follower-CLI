3f5acd2b43827e16b76d9d6fc8983c7feedf53a7
talum <tracidini@gmail.com>
2015-10-14 18:49:42 -0400

    Adds working Twitter methods

--- flatiron-twitter-cli/lib/twitter_cli.rb
+++ flatiron-twitter-cli/lib/twitter_cli.rb
@@ -1,4 +1,16 @@
-class TwitterCli
+require_relative '../config/environment'
 
-  
+class TwitterCli
+  def call 
+    puts "What do you want to do?"
+    command = gets.chomp.to_i
+    if command == 1
+      scrape = Scraper.new.username_array
+      twitter = TwitterApi.new
+      twitter.follow_all(scrape)
+      puts "You have followed everyone."
+    end
+  end
 end
+
+TwitterCli.new.call
