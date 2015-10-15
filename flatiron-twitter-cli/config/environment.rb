867642a547bb9fd1d9a9bd283ee2f2c8ada9e31d
NColey <ncoley>
2015-10-15 19:54:24 -0400

    adds views and controller to environment

--- flatiron-twitter-cli/config/environment.rb
+++ flatiron-twitter-cli/config/environment.rb
@@ -6,4 +6,8 @@ require 'yaml'
 
 require_relative '../lib/twitter.rb'
 require_relative '../lib/scraper.rb'
+require_relative '../views/follow/follow_everyone.rb'
+require_relative '../views/tweet/choose_a_classmate_to_tweet.rb'
+require_relative '../controllers/application_controller.rb'
 require_relative '../lib/TwitterCli.rb'
+
