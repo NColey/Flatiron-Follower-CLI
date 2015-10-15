361680b2022f0ea74dc5b73ed873ab3bbaf6ddfc
NColey <ncoley>
2015-10-15 10:30:32 -0400

    cleans up cli methods

--- flatiron-twitter-cli/lib/TwitterCli.rb
+++ flatiron-twitter-cli/lib/TwitterCli.rb
@@ -1,49 +1,94 @@
 class TwitterCli
-  attr_accessor :scrape, :twitter
-  def call
-    puts "Welcome to Flatiron Follower!"
-    puts "Please enter your Twitter handle."
-    twitter_handle = gets.chomp 
-    
-    puts <<-EOS
-    Here are your options:
+    attr_accessor :scraper, :twitter
 
-    1. FOLLOW
-    2. TWEET
-    3. VIEW
-    4. UNFOLLOW
-    EOS
+    def call
+        puts "Welcome to Flatiron Follower!"
 
-    puts "Please enter a command."
-    command = gets.chomp.to_i
-    @scrape = Scraper.new
-    @twitter = TwitterApi.new
-    case command
-      when 1
-        array = scrape.username_array
-        twitter.follow_all(array)
+        puts "Please enter your Twitter handle."
+        twitter_handle = gets.chomp 
+        options
+    end
+
+    def options
+        puts <<-EOS
+        Here are your options:
+
+        1. FOLLOW ALL CLASSMATES
+        2. FOLLOW A SINGLE CLASSMATE
+        3. TWEET
+        4. VIEW
+        5. UNFOLLOW ALL CLASSMATES
+        6. UNFOLLOW A SINGLE CLASSMATE
+        EOS
+        select_option
+    end
+
+    def select_option
+        puts "Please enter a command."
+        command = gets.chomp.to_i
+        @scrape = Scraper.new
+        @twitter = TwitterApi.new
+
+        case command
+        when 1
+            follow_your_classmates
+        when 2
+            follow_one_classmate
+        when 3
+            tweet
+        when 4
+            view_timeline
+        when 5
+            unfollow_all_classmates
+        when 6
+            unfollow_one_classmate
+        end
+    end
+
+    def follow_your_classmates
+        username_array = scrape.username_array
+        twitter.follow(username_array)
         puts "You have followed everyone."
-        #for individual case  twitter.follow_classmate(name)
-      when 2
-        puts "Who would you like to tweet?"
+
+    end
+
+    def follow_one_classmate
+        puts "Please enter the name of the classmate you want to follow."
+        name = gets.chomp.downcase
+        #convert to twitter handle
+        twitter.follow(twitter_handle)
+        puts "You have followed #{twitter_handle}."
+    end
+
+    def tweet
+        puts "Please enter the name of the classmate you want to tweet at."
+        name = gets.chomp.downcase
+        #convert to twitter handle
+        puts "Please enter your tweet"
+        message = gets.chomp
+        tweet = "@#{twitter_handle} #{message}"
+        twitter.tweet_at_classmate(tweet)
+    end
+
+    def view_timeline
+        puts "Whose tweets would you like to view? Enter a classmates name."
         name = gets.chomp
-        handle = handle_name #convert the name to handle 
-        puts "What would you like to say?"
-        message = gets.chomp 
-        message_student = "@{#{handle} {message}"
-        tweet.tweet_at_classmate(handle, message)       
-      when 3
-        puts "Whose tweets would you like to view?"
-        name = gets.chomp
-        handle = handle_name #convert the name to handle
-        twitter.classmate_timeline(username)
-      when 4
-        array = scrape.username_array
-        twitter.unfollow_all(array)
+        #convert name to twitter handle
+        twitter.classmate_timeline(twitter_handle)
+    end
+
+    def unfollow_all_classmates
+        username_array = scrape.username_array
+        twitter.unfollow(username_array)
         puts "You've unfollowed everyone."
     end
 
-  end
+    def unfollow_one_classmate
+        puts "Please enter the name of the classmate you want to unfollow."
+        name = gets.chomp.downcase
+        #convert to twitter handle
+        twitter.unfollow(twitter_handle)
+    end
 
 end
 
