1a7185bc87292c6df1a47522e15b1d783e2a96f8
NColey <ncoley>
2015-10-15 19:55:03 -0400

    updates cli methods and moves relevent data do controller

--- flatiron-twitter-cli/lib/TwitterCli.rb
+++ flatiron-twitter-cli/lib/TwitterCli.rb
@@ -1,92 +1,63 @@
 class TwitterCli
-    attr_accessor :scrape, :twitter
+    attr_accessor :quit, :controller
 
     def call
+        @quit = false 
         puts "Welcome to Flatiron Follower!"
         puts "Please enter your Twitter handle."
         twitter_handle = gets.chomp 
+        puts "Please wait while we load your information..."
+        @controller = Controller.new(twitter_handle)
         options
     end
 
     def options
-        puts <<-EOS
-        Here are your options:
+        while quit ==false
+            puts <<-EOS
+            Here are your options:
 
-        1. FOLLOW ALL CLASSMATES
-        2. FOLLOW A SINGLE CLASSMATE
-        3. TWEET
-        4. VIEW
-        5. UNFOLLOW ALL CLASSMATES
-        6. UNFOLLOW A SINGLE CLASSMATE
-        EOS
-        select_option
+            1. FOLLOW ALL CLASSMATES
+            2. FOLLOW A SINGLE CLASSMATE
+            3. TWEET
+            4. VIEW
+            5. UNFOLLOW ALL CLASSMATES
+            6. UNFOLLOW A SINGLE CLASSMATE
+            7. Exit
+            EOS
+            select_option
+            sleep(5)
+        end
     end
 
     def select_option
         puts "Please enter a command."
         command = gets.chomp.to_i
-        @scrape = Scraper.new
-        @twitter = TwitterApi.new
 
         case command
         when 1
-            follow_your_classmates
+            controller.follow_your_classmates
         when 2
-            follow_one_classmate
+            controller.follow_one_classmate
         when 3
-            tweet
+            controller.tweet
         when 4
-            view_timeline
+            controller.view_timeline
         when 5
-            unfollow_all_classmates
+            controller.unfollow_all_classmates
         when 6
-            unfollow_one_classmate
+            controller.unfollow_one_classmate
+        when 7
+            puts "Goodbye!"
+            exit
+        else
+            puts "We don't understand that command, please try again"
+            select_option
         end
     end
 
-    def follow_your_classmates
-        username_array = scrape.username_array
-        twitter.follow(username_array)
-        puts "You have followed everyone."
-
-    end
-
-    def follow_one_classmate
-        puts "Please enter the name of the classmate you want to follow."
-        name = gets.chomp
-        twitter_handle = scrape.twitter_username_hash[name]
-        twitter.follow(twitter_handle)
-        puts "You have followed #{twitter_handle}."
-    end
-
-    def tweet
-        puts "Please enter the name of the classmate you want to tweet at."
-        name = gets.chomp
-        twitter_handle = scrape.twitter_username_hash[name]
-        puts "Please enter your tweet"
-        message = gets.chomp
-        tweet = "@#{twitter_handle} #{message}"
-        twitter.tweet_at_classmate(tweet)
-    end
-
-    def view_timeline
-        puts "Whose tweets would you like to view? Enter a classmates name."
-        name = gets.chomp
-        twitter_handle = scrape.twitter_username_hash[name]
-        twitter.classmate_timeline(twitter_handle)
-    end
-
-    def unfollow_all_classmates
-        username_array = scrape.username_array
-        twitter.unfollow(username_array)
-        puts "You've unfollowed everyone."
-    end
-
-    def unfollow_one_classmate
-        puts "Please enter the name of the classmate you want to unfollow."
-        name = gets.chomp
-        twitter_handle = scrape.twitter_username_hash[name]
-        twitter.unfollow(twitter_handle)
+    def exit
+        quit = true
+        exit
     end
 
 end
