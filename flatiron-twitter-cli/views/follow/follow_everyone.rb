b79f0731d78ab39e4adce59a2f7a0496af83ab7f
NColey <ncoley>
2015-10-15 19:56:34 -0400

    builds views folder

---
+++ flatiron-twitter-cli/views/follow/follow_everyone.rb
@@ -0,0 +1,5 @@
+class FollowEveryone
+	def render
+		puts "You have followed everyone."
+	end
+end
---
+++ flatiron-twitter-cli/views/tweet/choose_a_classmate_to_tweet.rb
@@ -0,0 +1,5 @@
+class ChooseAClassmateToTweetAt
+	def render
+		puts "Please enter the name of the classmate you want to tweet at."
+	end
+end
